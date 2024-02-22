d1fc1d2e672f4d29deb77f2c9d7c514957de6954
victorprens <vprens12@gmail.com>
2024-02-22 11:35:01 -0500

    Creates Turtle Races script.

---
+++ main.py
@@ -0,0 +1,44 @@
+from turtle import Turtle, Screen
+from random import randint
+
+offset = 30
+x, y = (-230, 100)
+is_race_on = False
+race_ = []
+colors = ["red", "green", "orange", "yellow", "blue", "purple"]
+
+screen = Screen()
+
+screen.setup(width=500, height=400, startx=offset, starty=offset)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+
+def create_turtles():
+    turtles = [Turtle(shape="turtle") for _ in range(6)]
+    return turtles
+
+def create_starting_line(turtles):
+    global x, y
+    for index, turtle in enumerate(turtles):
+        turtle.penup()
+        turtle.color(colors[index])
+        turtle.goto(x, y)
+        y += -30
+
+turtles = create_turtles()
+create_starting_line(turtles=turtles)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in turtles:
+        print(turtle.pos()[0])
+        turtle.forward(randint(0, 10))
+        if turtle.pos()[0] >= 220:
+            print(f"La tortuga {turtle.color()[0]} gana.")
+            is_race_on = False
+            break
+
+
+
+screen.exitonclick()
